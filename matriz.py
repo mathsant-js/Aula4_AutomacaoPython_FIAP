d1fcd276376b92fcaca13409eee5cf7388ebff2e
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-04-27 11:22:34 -0300

    feat: exibindo valores com for e trabalhando com matrizes

---
+++ matriz.py
@@ -0,0 +1,14 @@
+for i in range(3):
+    for j in range(3):
+        print(f"{i} {j}")
+
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    
+]
+
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        print(f"Elemento[{i}][{j}]: {matriz[i][j]}")
